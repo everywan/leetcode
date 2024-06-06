0b960fe69a3ffde57d882e67baae8a87e21f68db
wzs <zhensheng.five@gmail.com>
2024-06-06 19:27:02 +0800

    优化代码

--- codes/763_partitionLabels.go
+++ codes/763_partitionLabels.go
@@ -20,39 +20,23 @@ package codes
 输出：[10]
 */
 
+// tag: 不简洁-已调整
 // 2024-06-06 start: 18:48 end:19:14 一遍过
 func partitionLabels(s string) []int {
-	if s == "" {
-		return []int{}
+	lastPos := [26]int{}
+	for i, c := range s {
+		lastPos[c-'a'] = i
 	}
-	// 每个字符的最远位置
-	set := make(map[byte]int, 26)
-	for i := 0; i < len(s); i++ {
-		set[s[i]] = i
-	}
-
 	result := []int{}
-	start := 0
-	endPos := set[s[0]]
-	for i := 1; i < len(s); i++ {
-		if i < endPos {
-			if set[s[i]] > endPos {
-				endPos = set[s[i]]
-			}
-			continue
+	start, end := 0, 0
+	for i, c := range s {
+		if end < lastPos[c-'a'] {
+			end = lastPos[c-'a']
 		}
-		result = append(result, endPos-start+1)
-		if i == endPos {
-			if i == len(s)-1 {
-				return result
-			}
+		if i == end {
+			result = append(result, end-start+1)
 			start = i + 1
-			endPos = set[s[i+1]]
-		} else {
-			start = i
-			endPos = set[s[i]]
 		}
 	}
-	result = append(result, endPos-start+1)
 	return result
 }
